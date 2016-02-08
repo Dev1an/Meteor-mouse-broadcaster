8a281ff1d257ec023aab879db833c77cd62edb6f
Devian <info@devian.tk>
2016-02-08 15:27:01 +0100

    init

---
+++ Meteor test.js
@@ -0,0 +1,25 @@
+Events = new Mongo.Collection('events')
+
+if (Meteor.isClient) {
+	Template.body.events({
+		'mousemove .canvas'(event) {
+			Events.insert({
+				name: 'moved',
+				position: [event.clientX, event.clientY, 10],
+				date: new Date()
+			})
+		}
+	})
+
+	Template.body.helpers({
+		lastEvent() { return Events.findOne() }
+	})
+
+	Meteor.subscribe('events')
+}
+
+if (Meteor.isServer) {
+	Meteor.publish('events', function() {
+		return Events.find({}, {sort: {date: -1}, limit: 1})
+	})
+}
